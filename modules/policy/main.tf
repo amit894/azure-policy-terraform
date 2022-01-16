7c36b7c22ab20ec63467e0a2cc63e6f3111737d2
Amit Raj <amit.894@gmail.com>
2022-01-16 14:12:45 +0530

    Adding basic location policy

--- modules/policy/main.tf
+++ modules/policy/main.tf
@@ -10,3 +10,65 @@ terraform {
 provider "azurerm" {
   features {}
 }
+
+resource "azurerm_resource_group" "sample" {
+  name     = "medium-blog"
+  location = "West Europe"
+}
+
+resource "azurerm_policy_definition" "location_policy" {
+  name         = "locRestrictPolicy"
+  policy_type  = "Custom"
+  mode         = "Indexed"
+  display_name = "Location Restriction Policy"
+
+  metadata = <<METADATA
+    {
+    "category": "General"
+    }
+
+METADATA
+
+
+  policy_rule = <<POLICY_RULE
+    {
+    "if": {
+      "not": {
+        "field": "location",
+        "in": "[parameters('allowedLocations')]"
+      }
+    },
+    "then": {
+      "effect": "audit"
+    }
+  }
+POLICY_RULE
+
+
+  parameters = <<PARAMETERS
+    {
+    "allowedLocations": {
+      "type": "Array",
+      "metadata": {
+        "description": "The list of allowed locations for resources.",
+        "displayName": "Allowed locations",
+        "strongType": "location"
+      }
+    }
+  }
+PARAMETERS
+
+}
+
+resource "azurerm_resource_group_policy_assignment" "location_policy_assigment" {
+  name                 = "locRestrictAssign"
+  resource_group_id    = azurerm_resource_group.sample.id
+  policy_definition_id = azurerm_policy_definition.location_policy.id
+  parameters = <<PARAMETERS
+  {
+      "allowedLocations": {
+      "value": ["southcentralus"]
+    }
+  }
+  PARAMETERS
+}
